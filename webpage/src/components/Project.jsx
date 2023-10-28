7d06cae5bba00afd7e331f15585065a5cafff5d1
Josh Pearlman <82927774+joshpman@users.noreply.github.com>
2023-10-28 00:29:43 -0400

    Projects section started

--- webpage/src/components/Project.jsx
+++ webpage/src/components/Project.jsx
@@ -1,7 +1,14 @@
 import weatherPhoto from '../images/projectPhotos/weather.webp';
+import quizPhoto from '../images/projectPhotos/Quiz.webp';
+import schnackPhoto from '../images/projectPhotos/schnack.webp';
+import formPhoto from '../images/projectPhotos/form.webp';
+import ProjectInsert from './ProjectInsert';
+import '../styles/projects.css';
+import { useState } from 'react';
 class ProjectItem{
-    constructor(name, description, languages, urls, photo){
+    constructor(name, date, description, languages, urls, photo,){
       this.name  = name;
+      this.date = date;
       this.description = description;
       this.languages = languages;
       this.urls = urls;
@@ -10,19 +17,51 @@ class ProjectItem{
 }
 const Weather = new ProjectItem(
     "Weather App",
+    "September 2023",
     "Weather App written with HTML, CSS, and JS. Utilized LocationIQ and OpenMETEO Api",
     ["HTML","JavaScript", "CSS","APIs"],
-    "PUT URL HERE",
-    {weatherPhoto}
-)
+    ["URL 1", "URL 2"],
+    weatherPhoto
+);
+const Quiz = new ProjectItem(
+    "Quiz Game",
+    "October 2023",
+    "Developed Single-Page Quiz Game with ReactJS, CSS, and OpenTDB API.",
+    ["HTML, CSS, ReactJS, APIs"],
+    ["URL 1", "URL 2"],
+    quizPhoto
+);
+const Schnack = new ProjectItem(
+    "Company Webpage Demo",
+    "August 2023",
+    "Developed Frontend for Schnack.io webpage.Utilzied HTML, CSS, and JavaScript",
+    ["HTML", "CSS", "Javascript"],
+    ["URL 1", "URL 2"],
+    schnackPhoto
+);
+const Form = new ProjectItem(
+    "Registration Form Demo",
+    "October 2023",
+    "3-step Registration page made with HTML, CSS, and JS. Validated input using custom written Regular Expressions.",
+    ["HTML", "CSS", "Javascript"],
+    ["URL 1", "URL 2"],
+    formPhoto
+);
+const projects = [Weather, Quiz, Schnack, Form];
 const Project = ()=>{
+    const [projectCount, setProjectCount] = useState(projects.length);
     return(
-        <div id="projectWrapper">
+        <section id="projects">
             <div id="projectWrapperInner">
                 <h1 id="projectsLabel">PROJECTS</h1>
                 <div id="projectSubheader">Each project represents a unique skill I've acquired</div>
+                <div id="actualProjects">
+                {projects.map((project, index)=>{
+                    return <ProjectInsert project={project} count={index+1} key={index}/>
+                })}
+                </div>
             </div>
-        </div>
+        </section>
     )
 }
 
--- webpage/src/components/ProjectInsert.jsx
+++ webpage/src/components/ProjectInsert.jsx
@@ -1,8 +1,57 @@
 
-const ProjectInsert = ({project})=>{
-    return(
-        <div className="ProjectWrapper">
+    const ProjectInsert = ({project, count})=>{
+    console.log(project);
+    console.log(count);
+    const LangTag = ({languages})=>{
+        return(
+            <div className="projectLanguagesWrapper">
+                <h1 className="projectLanguageH">{languages}</h1>
+            </div>
+        )
+    }
+    const LeftCheck= ({item, value})=>{
+        if(value%2===1){
+            return(
+                <div className="projectWrapperInner">
+                    <img src={item.photo} className="projectImage" alt=""></img>
+                    <div className="projectContentWrapper">
+                        <h1 className="projectHeader">{item.name}</h1>
+                        <h2 className="projectDate">{item.date}</h2>
+                        <p className="projectDescription">{item.description}</p>
+                        {
+                            item.languages.map((langs)=><LangTag languages={langs}/>)
+                        }
+                        <footer className="projectLogosBottom">
+                            <a href={item.urls[0]} className="codeButton">Code</a>
+                            <a href={item.urls[1]} className="repoButton">Live Webpage</a>
+                        </footer>
 
+                    </div>
+                </div>
+            )
+        }
+        return(
+            <div className="projectWrapperInner">
+                <div className="projectContentWrapper">
+                    <h1 className="projectHeader">{item.name}</h1>
+                    <h2 className="projectDate">{item.date}</h2>
+                    <p className="projectDescription">{item.description}</p>
+                    {
+                        item.languages.map((langs)=><LangTag languages={langs}/>)
+                    }
+                    <footer className="projectLogosBottom">
+                        <a href={item.urls[0]} className="codeButton">Code</a>
+                        <a href={item.urls[1]} className="repoButton">Live Webpage</a>
+                    </footer>
+
+                </div>
+                <img src={item.photo} className="projectImage" alt=""></img>
+            </div>
+        )
+    }
+    return(
+        <div className="projectWrapper">
+                <LeftCheck item={project} value={count}/>
         </div>
     )
 }
