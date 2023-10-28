e4ada35ee7345703bfb686235dfc28c8798f54f6
Josh Pearlman <82927774+joshpman@users.noreply.github.com>
2023-10-27 20:44:07 -0400

    About restructuring

--- webpage/src/About.jsx
+++ webpage/src/About.jsx
@@ -2,6 +2,7 @@ import './styles/about.css';
 import photo from "./images/aboutMePic.webp";
 const About = ()=>{
     return(
+        <div id="aboutContainer">
             <div id="aboutWrapper">
                 <div id="aboutLeft">
                     <img src={photo} draggable={false} alt="Something broke" id="aboutLeftImage"></img>
@@ -9,9 +10,10 @@ const About = ()=>{
                 <div id="aboutRight">
                     <h2 id="aboutMeLabel">ABOUT ME</h2>
                     <h1 id="aboutMeHeader">A Dedicated Student and Front-End Developer based in Michigan&#128205; </h1>
-                    <p id="aboutPara">As a dedicated Computer Science student, I have honed my skills in HTML, CSS, JavaScript, React, as well as Object-Oriented programming with languages like Java, C, and Assembly (MIPS). I excel at creating responsive websites that put user experience at the forefront. My strengths lie in developing intuitive, dynamic user interfaces with clean and efficient code, always staying updated with the latest development practices. While I prioritize my academic commitments, I am also a team player, ready to collaborate and bring my knowledge to any group endeavor to achieve excellence in web and software solutions.</p>
+                    <p id="aboutPara">As a dedicated Computer Science student, I've honed my expertise in a spectrum of programming languages, from the high-level Object-Oriented paradigms of Java to the intricate, low-level nuances of C and Assembly. My core strength, however, lies in crafting user-centric responsive websites using HTML, CSS, JavaScript, and React. Through clear and efficient code. I develop a deep understanding of the foundational principles behind each platform. Although I often work independently, my nature as a team player is evident. I'm always open to sharing my knowledge and insights to foster a collaborative and enriching learning environment.</p>
                 </div>
             </div>
+        </div>
 
     ) 
 }
